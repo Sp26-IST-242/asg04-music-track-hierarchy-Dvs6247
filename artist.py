3f49eba23f7d3725c81477cb7fc41895d2f85193
Dvs6247 <dvs6247@psu.edu>
2026-04-01 09:27:15 -0400

    Added to the README file with my name as the developer and the tester jake. Also added a class Artist, providing the name and genre of that song being played

--- artist.py
+++ artist.py
@@ -6,3 +6,29 @@ It introduces two core Python OOP conventions:
   1. The single leading-underscore (_name) signals a non-public attribute.
   2. @property exposes a clean public getter without allowing direct mutation.
 """
+
+class Artist:
+    
+    def __init__(self, name: str, genre: str):
+        '''
+        Initializes the Artist with a name and genre.
+        '''
+        self._name = name
+        self._genre  = genre
+
+        @property
+        def name(self) -> str:
+          '''The artist or show name.'''
+          return self._name
+        
+        @property
+        def __str__(self) -> str:
+           '''
+           Returns a string representation: <name>, <genre>
+           '''
+           return f"{self._name}, {self._genre}"
+        
+        #Example:
+        #artist = Artist("Kendrick Lamar", "Hip-Hop")
+        #print(artist)
+        #print(artist.name)
